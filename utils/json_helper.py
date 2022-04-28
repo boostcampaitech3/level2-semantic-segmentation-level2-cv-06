f44b574198598e79dd718a9e3b5256b204a99095
smg <alsrl8@naver.com>
2022-04-28 20:13:16 +0900

    draw polygons with annotations info

--- utils/json_helper.py
+++ utils/json_helper.py
@@ -22,14 +22,16 @@ class JsonHelper:
             self.__classes = [class_name for class_id, class_name in self.__classes]
 
         print("mapping images information to annotations")
-        self.__image_annotations = {}
+        self.__image_annotations = defaultdict(list)
+        self.__image_annotations_by_categories = {}
         for image in self.__images:
-            self.__image_annotations.update({image["id"]: defaultdict(list)})
+            self.__image_annotations_by_categories.update({image["id"]: defaultdict(list)})
         for annotation in self.__annotations:
             img_id = annotation["image_id"]
             cat_id = annotation["category_id"]
             anno_id = annotation["id"]
-            self.__image_annotations[img_id][cat_id].append(anno_id)
+            self.__image_annotations_by_categories[img_id][cat_id].append(anno_id)
+            self.__image_annotations[img_id].append(anno_id)
 
         print("finished initializing json helper")
         
@@ -41,8 +43,44 @@ class JsonHelper:
 
     def get_image_list(self) -> list:
         return [{"id": img["id"], "file_name": img["file_name"]} for img in self.__images]
-            
 
+    def showAnns(self, anns, draw_bbox=False):
+        import matplotlib.pyplot as plt
+        from matplotlib.collections import PatchCollection
+        from matplotlib.patches import Polygon
+        import numpy as np
+
+        ax = plt.gca()  # TODO should search what this is
+        ax.set_autoscale_on(False)
+        polygons = []
+        color = []
+        for ann in anns:
+            c = (np.random.random((1, 3)) * 0.6 + 0.4).tolist()[0]
+            # segmentation
+            # polygon
+            for seg in ann["segmentation"]:
+                poly = np.array(seg).reshape((int(len(seg)/2), 2))
+                polygons.append(Polygon(poly))
+                color.append(c)
+        p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.4)
+        ax.add_collection(p)
+        p = PatchCollection(polygons, facecolor='none', edgecolors=color, linewidths=2)
+        ax.add_collection(p)
+
+    def get_annotations(self, img_id: int, category_id=None) -> list:
+        """
+        Return
+        ---
+        list of annotations
+        """
+        if not category_id:
+            anno_ids = set(self.__image_annotations[img_id])
+        else:
+            anno_ids = set(self.__image_annotations_by_categories[img_id][category_id])
+        return [anno for anno in self.__annotations if anno["id"] in anno_ids]
+            
+        
+            
 if __name__ == '__main__':
     # test code
     classes = ["Gene"]
