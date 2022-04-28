0269900d661258238e8486225641d30a19573775
pyhonic <pyhonic@gmail.com>
2022-04-28 15:25:38 +0900

    [REFACT] id들이 sort 되어있는지 확인

--- utils/concat_json.py
+++ utils/concat_json.py
@@ -22,15 +22,56 @@ def main(args):
     new_json['info'] = train_data['info'].copy()
     new_json['licenses'] = train_data['licenses'].copy()
     new_json['images'] = []
-    new_json['categories'] = train_data['categories']
+    new_json['categories'] = train_data['categories'].copy()
     new_json['annotations'] = []
+
+    # before_sort_anns = [train_ann['id'] for train_ann in train_anns]
+    # print(id(before_sort_anns))
+
+    # train_anns = sorted(train_anns, key=lambda x : x['id'])
+    # after_sort_anns = [train_ann['id'] for train_ann in train_anns]
+    # print(id(after_sort_anns))
+
+    # if after_sort_anns == before_sort_anns:
+    #     print("train_anns are same order")
+
+    # before_sort_imgs = [train_img['id'] for train_img in train_imgs]
+    # print(id(before_sort_imgs))
+
+    # concat_imgs = sorted(concat_imgs, key=lambda x : x['id'])
+    # after_sort_imgs = [train_img['id'] for train_img in train_imgs]
+    # print(id(after_sort_imgs))
+
+    # if after_sort_imgs == before_sort_imgs:
+    #     print("train_imgs are same order")
+
+    # before_sort_imgs = [concat_img['id'] for concat_img in concat_imgs]
+    # print(id(before_sort_imgs))
+
+    # train_imgs = sorted(train_imgs, key=lambda x : x['id'])
+    # after_sort_imgs = [concat_img['id'] for concat_img in concat_imgs]
+    # print(id(after_sort_imgs))
+
+    # if after_sort_imgs == before_sort_imgs:
+    #     print("concat_imgs are same order")
+
+    # before_sort_anns = [concat_ann['id'] for concat_ann in concat_anns]
+    # print(id(before_sort_imgs))
+    
+    # concat_anns = sorted(concat_anns, key=lambda x : x['id'])
+    # after_sort_anns = [concat_ann['id'] for concat_ann in concat_anns]
+    # print(id(after_sort_imgs))
+
+    # if after_sort_anns == before_sort_anns:
+    #     print("concat_anns are same order")
+
 
     for idx, train_img in enumerate(train_imgs):
         file_name = train_img['file_name']
         img_id = train_img['id']
         if file_name.startswith('batch_03'):
             new_json['images'].extend(train_imgs[:idx])
-            
+
             for train_ann in train_anns:
                 image_id = train_ann['image_id']
                 ann_id = train_ann['id']
@@ -39,11 +80,13 @@ def main(args):
                 else:
                     new_json['annotations'].append(train_ann)
             break
+    
+    # print(img_id, ann_id)
 
     for concat_img in concat_imgs:
         concat_img['id'] += img_id
         new_json['images'].append(concat_img)
-    
+
     for concat_ann in concat_anns:
         concat_ann['image_id'] += img_id
         concat_ann['id'] += ann_id
@@ -52,7 +95,7 @@ def main(args):
 
     with open(osp.join(save_dir, 'train_total.json'), 'w') as f:
         json.dump(new_json, f, indent=4)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
